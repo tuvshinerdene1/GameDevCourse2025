dddc86463acbeaece38602247f855395bddc6cec
tuvshin.erdene <tuvshin.erdene25@gmail.com>
2025-09-11 19:01:10 +0800

    block is falling

--- 3d-tetris/main.gd
+++ 3d-tetris/main.gd
@@ -1,11 +1,18 @@
 extends Node
-
+var block = preload('res://blocks/block.tscn')
+var instance = block.instantiate()
+var spawnPoint
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	spawnPoint = get_node("spawnPoint")
+	add_child(instance)
+	instance.global_position = spawnPoint.global_position
 
 
+func _clear():
+	instance.queue_free()
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
